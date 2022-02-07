c888869e3b3fba35500a546de1174411567653d5
Peter Dias <peter@moodle.com>
2022-02-07 15:07:13 +0800

    Initial commit

---
+++ database/factories/InstrumentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Instrument;
+use Faker\Generator as Faker;
+
+$factory->define(Instrument::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
